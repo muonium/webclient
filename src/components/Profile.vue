bb2415bd5aa595cba7c706ccf41e35b619b43e3e
hitoshi54 <dylanclement7@gmail.com>
2018-03-20 12:27:45 +0100

    Profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -29,22 +29,22 @@
           <form>
             <h3>{{ $t('Profile.changelogin') }}</h3>
             <p class="input-large">
-              <input type="text" id="new_login" :placeholder="$t('Profile.newlogin')" required>
+              <input type="text" id="new_login" :placeholder="$t('Profile.newlogin')" v-model="fields.changeLogin.login" required>
               <label class="fa fa-user" for="new_login" aria-hidden="true"></label>
             </p>
-            <input type="submit" class="btn btn-required btn-profile" @click="changeLogin" :value="$t('Global.submit')" disabled>
-            <div id="changeLoginReturn"></div>
+            <input type="submit" class="btn btn-required btn-profile" @click.prevent="changeLogin" :value="$t('Global.submit')" :disabled="!isComplete('changeLogin')">
+            <div v-if="this.changeLoginReturn">{{ $t('this.changeLoginReturn') }}</div>
           </form>
         </div>
         <div>
           <form>
             <h3>{{ $t('Profile.changemail') }}</h3>
             <p class="input-large">
-              <input type="text" id="new_mail" :placeholder="$t('Profile.changemail')" required>
+              <input type="text" id="new_mail" :placeholder="$t('Profile.changemail')" v-model="fields.changeMail.mail" required>
               <label class="fa fa-envelope" for="new_mail" aria-hidden="true"></label>
             </p>
-            <input type="submit" class="btn btn-required btn-profile" @click="changeMail" :value="$t('Global.submit')" disabled>
-            <div id="changeMailReturn"></div>
+            <input type="submit" class="btn btn-required btn-profile" @click.prevent="changeMail" :value="$t('Global.submit')" :disabled="!isComplete('changeMail')">
+            <div v-if="this.changeMailReturn">{{ $t('this.changeMailReturn') }}</div>
           </form>
         </div>
       </div>
@@ -57,38 +57,38 @@
           <form>
             <h3>{{ $t('Profile.changepwd') }}</h3>
             <p class="input-large">
-              <input type="password" id="old_pwd" :placeholder="$t('Profile.oldpwd')" required>
+              <input type="password" id="old_pwd" :placeholder="$t('Profile.oldpwd')" v-model="fields.changePassword.old_pwd" required>
               <label class="fa fa-lock" for="old_pwd" aria-hidden="true"></label>
             </p>
             <p class="input-large">
-              <input type="password" id="new_pwd" :placeholder="$t('Profile.newpwd')" required>
+              <input type="password" id="new_pwd" :placeholder="$t('Profile.newpwd')" v-model="fields.changePassword.new_pwd" required>
               <label class="fa fa-lock" for="new_pwd" aria-hidden="true"></label>
             </p>
             <p class="input-large">
-              <input type="password" id="pwd_confirm" :placeholder="$t('Register.confirm')" required>
+              <input type="password" id="pwd_confirm" :placeholder="$t('Register.confirm')" v-model="fields.changePassword.pwd_confirm" required>
               <label class="fa fa-lock" for="pwd_confirm" aria-hidden="true"></label>
             </p>
-            <input type="submit" class="btn btn-required btn-profile" @click="changePassword" :value="$t('Global.submit')" disabled>
-            <div id="changePasswordReturn"></div>
+            <input type="submit" class="btn btn-required btn-profile" @click.prevent="changePassword" :value="$t('Global.submit')" :disabled="!isComplete('changePassword')">
+            <div v-if="this.changePasswordReturn">{{ $t('this.changePasswordReturn') }}</div>
           </form>
         </div>
         <div>
           <form>
             <h3>{{ $t('Profile.changepp') }}</h3>
             <p class="input-large">
-              <input type="password" id="old_pp" :placeholder="$t('Profile.oldpp')" required>
+              <input type="password" id="old_pp" :placeholder="$t('Profile.oldpp')" v-model="fields.changeCek.old_pp" required>
               <label class="fa fa-lock" for="old_pp" aria-hidden="true"></label>
             </p>
             <p class="input-large">
-              <input type="password" id="new_pp" :placeholder="$t('Profile.newpp')" required>
+              <input type="password" id="new_pp" :placeholder="$t('Profile.newpp')" v-model="fields.changeCek.new_pp" required>
               <label class="fa fa-lock" for="new_pp" aria-hidden="true"></label>
             </p>
             <p class="input-large">
-              <input type="password" id="pp_confirm" :placeholder="$t('Register.confirm')" required>
+              <input type="password" id="pp_confirm" :placeholder="$t('Register.confirm')" v-model="fields.changeCek.pp_confirm" required>
               <label class="fa fa-lock" for="pp_confirm" aria-hidden="true"></label>
             </p>
-            <input type="submit" class="btn btn-required btn-profile" @click="changeCek" :value="$t('Global.submit')" disabled>
-            <div id="changePassPhraseReturn"></div>
+            <input type="submit" class="btn btn-required btn-profile" @click.prevent="changeCek" :value="$t('Global.submit')" :disabled="!isComplete('changeCek')">
+            <div v-if="this.changeCekReturn">{{ $t('this.changeCekReturn') }}</div>
           </form>
         </div>
       </div>
@@ -98,10 +98,10 @@
       <legend>{{ $t('Profile.otheroptions') }}</legend>
       <h3>{{ $t('Profile.theme') }}</h3>
       <p class="input-large">
-        <input type="radio" name="theme" id="light" @click="switchTheme">
+        <input type="radio" name="theme" id="light" @click="switchTheme('light')">
         <label for="light">Light</label>
 
-        <input type="radio" name="theme" id="dark" @click="switchTheme">
+        <input type="radio" name="theme" id="dark" @click="switchTheme('dark')">
         <label for="dark">Dark</label>
       </p>
     </fieldset>
@@ -109,27 +109,30 @@
     <fieldset>
       <h3>{{ $t('Profile.doubleAuth') }}</h3>
       <p class="input-large">
-        <input type="checkbox" @click="changeAuth" id="doubleAuth" :checked="doubleAuth">
+        <input type="checkbox" @click.prevent="changeAuth" id="doubleAuth" v-model="doubleAuth">
         <label for="doubleAuth">{{ $t('Register.doubleAuth') }}</label>
       </p>
-      <div id="changeAuthReturn"></div>
+      <div v-if="this.changeAuthReturn">{{ $t('this.changeAuthReturn') }}</div>
     </fieldset>
 
     <fieldset>
       <form>
         <h3>{{ $t('Profile.deleteAccount') }}</h3>
         <p class="input-large">
-          <input type="checkbox" id="delete" required>
+          <input type="checkbox" id="delete" v-model="deleteCheckbox" required>
           <label for="delete">{{ $t('Profile.iwant') }}</label>
         </p>
-        <input type="submit" class="btn btn-required btn-warning" @click="confirmDelete" :value="$t('Profile.deleteAccount')" disabled>
-        <div id="deleteUserReturn"></div>
+        <input type="submit" class="btn btn-required btn-warning" @click.prevent="confirmDelete" :value="$t('Profile.deleteAccount')" :disabled="!deleteCheckbox">
       </form>
     </fieldset>
   </div>
 </template>
 
 <script>
+import sjcl from 'sjcl'
+import base64 from 'hi-base64'
+import muiHash from '../mui_hash'
+
 export default {
   name: 'profile',
   data () {
@@ -138,23 +141,125 @@ export default {
       login: null,
       email: null,
       doubleAuth: false,
-      fields: {}
+      deleteCheckbox: false,
+      fields: {
+        changeLogin: {
+          login: ''
+        },
+        changeMail: {
+          mail: ''
+        },
+        changePassword: {
+          old_pwd: '',
+          new_pwd: '',
+          pwd_confirm: ''
+        },
+        changeCek: {
+          old_pp: '',
+          new_pp: '',
+          pp_confirm: ''
+        }
+      },
+      changeLoginReturn: null,
+      changeMailReturn: null,
+      changePasswordReturn: null,
+      changeCekReturn: null,
+      changeAuthReturn: null
     }
   },
   methods: {
-    changeLogin () {},
-    changeMail () {},
-    changePassword () {},
-    changeCek () {},
+    changeLogin () {
+      this.$http.post('user/changeLogin', {login: this.fields.changeLogin.login}).then((res) => {
+        this.changeLoginReturn = 'Profile.updateOk'
+      }, (res) => {
+        this.changeLoginReturn = 'Profile.updateErr'
+        switch (res.body.message) {
+          case 'loginExists':
+            this.changeLoginReturn = 'Profile.loginExists'
+            break
+          case 'loginFormat':
+            this.changeLoginReturn = 'Register.loginFormat'
+        }
+      })
+    },
+    changeMail () {
+      this.$http.post('user/changeMail', {mail: this.fields.changeMail.mail}).then((res) => {
+        this.changeMailReturn = 'Profile.updateOk'
+      }, (res) => {
+        this.changeMailReturn = 'Profile.updateErr'
+        switch (res.body.message) {
+          case 'mailExists':
+            this.changeMailReturn = 'Register.mailExists' // TODO: Update string
+            break
+          case 'mailFormat':
+            this.changeMailReturn = 'Profile.mailFormat'
+        }
+      })
+    },
+    changePassword () {
+      if (this.fields.changePassword.new_pwd.length < 6) {
+        this.changePasswordReturn = 'Register.passLength'
+      } else if (this.fields.changePassword.new_pwd !== this.fields.changePassword.pwd_confirm) {
+        this.changePasswordReturn = 'Register.badPassConfirm'
+      } else {
+        this.$http.post('user/changePassword', {old_pwd: muiHash(this.fields.changePassword.old_pwd), new_pwd: muiHash(this.fields.changePassword.new_pwd)}).then((res) => {
+          this.changePasswordReturn = 'Profile.updateOk'
+        }, (res) => {
+          this.changePasswordReturn = 'Profile.updateErr'
+        })
+      }
+    },
+    changeCek () {
+      let currentCek = sessionStorage.getItem('cek')
+      let currentKek = sessionStorage.getItem('kek')
+      let newKek = this.fields.changeCek.new_pp
+      if (this.fields.changeCek.old_pp !== currentKek) {
+        this.changeCekReturn = 'Profile.badOldPassphrase'
+      } else if (newKek.length < 6) {
+        this.changeCekReturn = 'Register.passLength'
+      } else if (newKek !== this.fields.changeCek.pp_confirm) {
+        this.changeCekReturn = 'Register.badPassphraseConfirm'
+      } else {
+        // crypto parameters
+        let a = sjcl.random.randomWords(4) // authentication data - 128 bits
+        let i = sjcl.random.randomWords(4) // initialization vector - 128 bits
+        let s = sjcl.random.randomWords(4) // salt - 128 bits
+        // encrypt it
+        let newCek = sjcl.encrypt(newKek, currentCek, {
+          mode: 'gcm', iv: i, salt: s, iter: 7000, ks: 256, adata: a, ts: 128
+        })
+        newCek = base64.encode(newCek) // don't store a Json in DB...
+
+        this.$http.post('user/changeCek', {cek: newCek}).then((res) => {
+          sessionStorage.setItem('cek', newCek)
+          sessionStorage.setItem('kek', newKek)
+          this.changeCekReturn = 'Profile.updateOk'
+        }, (res) => {
+          this.changeCekReturn = 'Profile.updateErr'
+        })
+      }
+    },
     switchTheme () {},
-    changeAuth () {},
-    confirmDelete () {}
-  },
-  computed: {
-    isComplete () {
+    changeAuth () {
+      this.$http.post('user/changeAuth').then((res) => {
+        this.changeAuthReturn = 'Profile.updateOk'
+      }, (res) => {
+        this.changeAuthReturn = 'Profile.updateErr'
+      })
+    },
+    confirmDelete () {
+      if (confirm(this.$t('Profile.accountDeletionConfirm'))) {
+        this.$http.delete('user').then((res) => {
+          // Deleted
+        }, (res) => {
+          // Error
+        })
+      }
+    },
+    isComplete (section) {
       let complete = true
-      for (let key of Object.keys(this.fields)) {
-        if (this.fields[key].required && (this.fields[key].value === '' || this.fields[key].value === false)) {
+      for (let key of Object.keys(this.fields[section])) {
+        if (this.fields[section][key] === '') {
           complete = false
           break
         }
@@ -167,6 +272,15 @@ export default {
       this.$parent.logout()
       return false
     }
+
+    this.$http.get('user').then((res) => {
+      this.id = res.body.data.id
+      this.login = res.body.data.login
+      this.email = res.body.data.email
+      this.doubleAuth = res.body.data.doubleAuth
+    }, (res) => {
+      // Error
+    })
   }
 }
 </script>
